78e18af748572177f6ff6c0e439c735c29f1fada
Yannis Gkoumas <ygkoumas@users.noreply.github.com>
2015-11-27 01:38:13 +0000

    github api to list the the projects

--- javascript/GithubCall.js
+++ javascript/GithubCall.js
@@ -1,1 +1,19 @@
-alert(1111);
+document.onload = function() {
+	githubCall = new XMLHttpRequest;
+
+	githubCall.onreadystatechange=function() {
+		if (githubCall.readyState == 4 && githubCall.status == 200) {
+			document.getElementsByClassName('github-call')[0].innerHTML = JSON.parse(githubCall.response);
+		}
+	};
+
+	githubCall.open('GET','https://api.github.com/users/ygkoumas/repos?callback=myjsonp');
+
+	githubCall.send();
+
+	myjsonp = function (res) {
+		return res.data[0].name;
+	};
+};
+
+
